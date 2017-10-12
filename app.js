1eb9f2cc11d11d5fac21e662a8032e3b810df871
Kev Comedia <kevcomedia@gmail.com>
2017-10-12 15:29:37 +0800

    Include software and language in JSON response

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.set('trust proxy', true);
+
 app.get('/', function(req, res) {
-  res.json({message: 'Hello World'});
+  const softwareRegExp = /\((.*)\)/;
+
+  const software = softwareRegExp.exec(req.headers['user-agent'])[1];
+  const language = req.headers['accept-language'].split(',')[0];
+
+  res.json({
+    ip: req.ip,
+    language,
+    software,
+  });
 });
 
 module.exports = app;
